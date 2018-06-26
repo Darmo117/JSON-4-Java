5b24df6f9bb8979ce214d924062ba849cd14bedd
Damien Vergnet <darmo117@gmail.com>
2018-06-26 09:47:35 +0200

    feat: added methods in object and array to get/add value more easily

--- src/main/java/net/darmo_creations/json/JsonArray.java
+++ src/main/java/net/darmo_creations/json/JsonArray.java
@@ -1,18 +1,18 @@
 /*
  * Copyright © 2018 Damien Vergnet
- * 
+ *
  * This file is part of JSON-4-Java.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -40,7 +40,7 @@ public final class JsonArray extends ArrayList<JsonEntity> implements JsonEntity
 
   /**
    * Creates an array from the given collection.
-   * 
+   *
    * @param c the source collection
    */
   public JsonArray(Collection<? extends JsonEntity> c) {
@@ -50,7 +50,7 @@ public final class JsonArray extends ArrayList<JsonEntity> implements JsonEntity
   /**
    * Returns the element at the specified position in this array. It will try to cast the element
    * into the given object type.
-   * 
+   *
    * @param index index of the element to return
    * @param type the object type into which the element has to be cast
    * @return the element at the specified position in this array
@@ -59,6 +59,135 @@ public final class JsonArray extends ArrayList<JsonEntity> implements JsonEntity
    */
   public <T extends JsonEntity> T getAs(int index, JsonEntityType<T> type) {
     return type.getEntityClass().cast(get(index));
+  }
+
+  /**
+   * Returns the object at the specified position in this array.
+   *
+   * @param index index of the element to return
+   * @return the object at the specified position in this array or null if the value is null
+   * @throws IndexOutOfBoundsException if the index is out of range (index &lt; 0 or index ≥ size())
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonObject}
+   */
+  public JsonObject getObject(int index) {
+    return getAs(index, JsonEntityType.OBJECT);
+  }
+
+  /**
+   * Returns the array at the specified position in this array.
+   *
+   * @param index index of the element to return
+   * @return the array at the specified position in this array or null if the value is null
+   * @throws IndexOutOfBoundsException if the index is out of range (index &lt; 0 or index ≥ size())
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonArray}
+   */
+  public JsonArray getArray(int index) {
+    return getAs(index, JsonEntityType.ARRAY);
+  }
+
+  /**
+   * Returns the boolean at the specified position in this array.
+   *
+   * @param index index of the element to return
+   * @return the boolean at the specified position in this array or null if the value is null
+   * @throws IndexOutOfBoundsException if the index is out of range (index &lt; 0 or index ≥ size())
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonBoolean}
+   */
+  public Boolean getBoolean(int index) {
+    JsonBoolean b = getAs(index, JsonEntityType.BOOLEAN_VALUE);
+    return b != null ? b.get() : null;
+  }
+
+  /**
+   * Appends the specified boolean to the end of this list.
+   *
+   * @param b element to be appended to this list
+   * @return true (as specified by {@link ArrayList#add})
+   */
+  public boolean addBoolean(boolean b) {
+    return add(new JsonBoolean(b));
+  }
+
+  /**
+   * Inserts the specified boolean at the specified position in this list. Shifts the element
+   * currently at that position (if any) and any subsequent elements to the right (adds one to their
+   * indices).
+   *
+   * @param index index at which the specified boolean is to be inserted
+   * @param b element to be inserted
+   */
+  public void addBoolean(int index, boolean b) {
+    add(index, new JsonBoolean(b));
+  }
+
+  /**
+   * Returns the number at the specified position in this array.
+   *
+   * @param index index of the element to return
+   * @return the number at the specified position in this array or null if the value is null
+   * @throws IndexOutOfBoundsException if the index is out of range (index &lt; 0 or index ≥ size())
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonNumber}
+   */
+  public Double getNumber(int index) {
+    JsonNumber n = getAs(index, JsonEntityType.NUMBER_VALUE);
+    return n != null ? n.get() : null;
+  }
+
+  /**
+   * Appends the specified number to the end of this list.
+   *
+   * @param d element to be appended to this list
+   * @return true (as specified by {@link ArrayList#add})
+   */
+  public boolean addNumber(double d) {
+    return add(new JsonNumber(d));
+  }
+
+  /**
+   * Inserts the specified number at the specified position in this list. Shifts the element
+   * currently at that position (if any) and any subsequent elements to the right (adds one to their
+   * indices).
+   *
+   * @param index index at which the specified boolean is to be inserted
+   * @param d element to be inserted
+   */
+  public void addNumber(int index, double d) {
+    add(index, new JsonNumber(d));
+  }
+
+  /**
+   * Returns the string at the specified position in this array.
+   *
+   * @param index index of the element to return
+   * @return the string at the specified position in this array or null if the value is null
+   * @throws IndexOutOfBoundsException if the index is out of range (index &lt; 0 or index ≥ size())
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonString}
+   */
+  public String getString(int index) {
+    JsonString s = getAs(index, JsonEntityType.STRING_VALUE);
+    return s != null ? s.get() : null;
+  }
+
+  /**
+   * Appends the specified string to the end of this list.
+   *
+   * @param s element to be appended to this list
+   * @return true (as specified by {@link ArrayList#add})
+   */
+  public boolean addString(String s) {
+    return add(new JsonString(s));
+  }
+
+  /**
+   * Inserts the specified string at the specified position in this list. Shifts the element
+   * currently at that position (if any) and any subsequent elements to the right (adds one to their
+   * indices).
+   *
+   * @param index index at which the specified boolean is to be inserted
+   * @param s element to be inserted
+   */
+  public void addString(int index, String s) {
+    add(index, new JsonString(s));
   }
 
   @Override
--- src/main/java/net/darmo_creations/json/JsonObject.java
+++ src/main/java/net/darmo_creations/json/JsonObject.java
@@ -1,18 +1,18 @@
 /*
  * Copyright © 2018 Damien Vergnet
- * 
+ *
  * This file is part of JSON-4-Java.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -41,7 +41,7 @@ public final class JsonObject extends HashMap<String, JsonEntity> implements Jso
 
   /**
    * Creates an object from the given map.
-   * 
+   *
    * @param m the source map
    */
   public JsonObject(Map<String, ? extends JsonEntity> m) {
@@ -49,13 +49,12 @@ public final class JsonObject extends HashMap<String, JsonEntity> implements Jso
   }
 
   /**
-   * Returns the entity to which the specified key is mapped, or null if this object contains no
-   * mapping for the key. It will try to cast the value into the given object type.
-   * 
+   * Returns the entity to which the specified key is mapped. It will try to cast the value into the
+   * given object type.
+   *
    * @param key the key whose associated value is to be returned
    * @param type the object type into which the entity has to be cast
-   * @return the entity to which the specified key is mapped, or null if this map contains no
-   *         mapping for the key
+   * @return the entity to which the specified key is mapped
    * @throws NoSuchElementException if there is no mapping for the key
    * @throws ClassCastException if the actual value is not assignable to the specified type
    */
@@ -63,6 +62,109 @@ public final class JsonObject extends HashMap<String, JsonEntity> implements Jso
     if (!containsKey(key))
       throw new NoSuchElementException("No mapping for key '" + key + "'!");
     return type.getEntityClass().cast(get(key));
+  }
+
+  /**
+   * Returns the object for the given key.
+   *
+   * @param key the key whose associated value is to be returned
+   * @return the object to which the specified key is mapped or null if the value is null
+   * @throws NoSuchElementException if there is no mapping for the key
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonObject}
+   */
+  public JsonObject getObject(String key) {
+    return getAs(key, JsonEntityType.OBJECT);
+  }
+
+  /**
+   * Returns the array for the given key.
+   *
+   * @param key the key whose associated value is to be returned
+   * @return the array to which the specified key is mapped or null if the value is null
+   * @throws NoSuchElementException if there is no mapping for the key
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonArray}
+   */
+  public JsonArray getArray(String key) {
+    return getAs(key, JsonEntityType.ARRAY);
+  }
+
+  /**
+   * Returns the boolean for the given key.
+   *
+   * @param key the key whose associated value is to be returned
+   * @return the boolean to which the specified key is mapped or null if the value is null
+   * @throws NoSuchElementException if there is no mapping for the key
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonBoolean}
+   */
+  public Boolean getBoolean(String key) {
+    JsonBoolean b = getAs(key, JsonEntityType.BOOLEAN_VALUE);
+    return b != null ? b.get() : null;
+  }
+
+  /**
+   * Associates the specified boolean value with the specified key in this map. If the map
+   * previously contained a mapping for the key, the old value is replaced.
+   *
+   * @param key key with which the specified value is to be associated
+   * @param b value to be associated with the specified key
+   * @return the previous value associated with key, or null if there was no mapping for key. (A
+   *         null return can also indicate that the map previously associated null with key.)
+   */
+  public JsonEntity putBoolean(String key, boolean b) {
+    return put(key, new JsonBoolean(b));
+  }
+
+  /**
+   * Returns the number for the given key.
+   *
+   * @param key the key whose associated value is to be returned
+   * @return the number to which the specified key is mapped or null if the value is null
+   * @throws NoSuchElementException if there is no mapping for the key
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonNumber}
+   */
+  public Double getNumber(String key) {
+    JsonNumber n = getAs(key, JsonEntityType.NUMBER_VALUE);
+    return n != null ? n.get() : null;
+  }
+
+  /**
+   * Associates the specified double value with the specified key in this map. If the map previously
+   * contained a mapping for the key, the old value is replaced.
+   *
+   * @param key key with which the specified value is to be associated
+   * @param d value to be associated with the specified key
+   * @return the previous value associated with key, or null if there was no mapping for key. (A
+   *         null return can also indicate that the map previously associated null with key.)
+   */
+  public JsonEntity putNumber(String key, double d) {
+    return put(key, new JsonNumber(d));
+  }
+
+  /**
+   * Returns the string for the given key.
+   *
+   * @param key the key whose associated value is to be returned
+   * @return the string to which the specified key is mapped or null if the value is null
+   * @throws NoSuchElementException if there is no mapping for the key
+   * @throws ClassCastException if the actual value is not assignable to a {@link JsonString}
+   */
+  public String getString(String key) {
+    JsonString s = getAs(key, JsonEntityType.STRING_VALUE);
+    return s != null ? s.get() : null;
+  }
+
+  /**
+   * Associates the specified string value with the specified key in this map. If the map previously
+   * contained a mapping for the key, the old value is replaced.
+   *
+   * @param key key with which the specified value is to be associated
+   * @param s value to be associated with the specified key
+   * @return the previous value associated with key, or null if there was no mapping for key. (A
+   *         null return can also indicate that the map previously associated null with key.)
+   * @throws NullPointerException if the value is null
+   */
+  public JsonEntity putString(String key, String s) {
+    return put(key, new JsonString(s));
   }
 
   @Override
