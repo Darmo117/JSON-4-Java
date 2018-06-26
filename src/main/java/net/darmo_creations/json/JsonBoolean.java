ec532c1a0c30184923dc6f7ea9a129a358478d40
Damien Vergnet <darmo117@gmail.com>
2018-06-26 09:48:04 +0200

    refactor: JsonBoolean and JsonNumber constructors take primitive types

--- src/main/java/net/darmo_creations/json/JsonBoolean.java
+++ src/main/java/net/darmo_creations/json/JsonBoolean.java
@@ -1,18 +1,18 @@
 /*
  * Copyright © 2018 Damien Vergnet
- * 
+ *
  * This file is part of JSON-4-Java.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,7 +27,7 @@ public final class JsonBoolean extends JsonValue<Boolean> {
   public static final String TYPE_NAME = "boolean";
   private static final long serialVersionUID = 7082720359306820854L;
 
-  public JsonBoolean(Boolean value) {
+  public JsonBoolean(boolean value) {
     super(value, "boolean");
   }
 }
--- src/main/java/net/darmo_creations/json/JsonNumber.java
+++ src/main/java/net/darmo_creations/json/JsonNumber.java
@@ -1,18 +1,18 @@
 /*
  * Copyright © 2018 Damien Vergnet
- * 
+ *
  * This file is part of JSON-4-Java.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,7 +27,7 @@ public final class JsonNumber extends JsonValue<Double> {
   public static final String TYPE_NAME = "number";
   private static final long serialVersionUID = 3006650913617176985L;
 
-  public JsonNumber(Double value) {
+  public JsonNumber(double value) {
     super(value, "number");
   }
 
